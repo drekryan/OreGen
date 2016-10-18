2a43c6d26122c0754dc2bb2710171abe3f5d88b5
Derrick Lambert <drekryan@yahoo.com>
2016-10-18 07:00:16 -0400

    Basic Plugin Structure

---
+++ README.md
@@ -0,0 +1,6 @@
+OreGen
+=======
+
+Author: [Drekryan](https://www.github.com/Drekryan)
+
+OreGen is a Minecraft Spigot Plugin which replaces Cobble Generation with Ore Generation for SkyBlock Servers
---
+++ src/main/java/com/Drekryan/OreGen/OreGen.java
@@ -0,0 +1,23 @@
+package com.Drekryan.OreGen;
+
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OreGen extends JavaPlugin {
+    private ConsoleCommandSender consoleSender;
+
+    @Override
+    public void onEnable() {
+        consoleSender = this.getServer().getConsoleSender();
+        if (consoleSender != null) {
+            consoleSender.sendMessage("[OreGen] Successfully Enabled!");
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        if (consoleSender != null) {
+            consoleSender.sendMessage("[OreGen] OreGen disabled...");
+        }
+    }
+}
